25bf45983b991eb46d2e71fa98072952df8a299a
MarlosAugusto <marlos.gomes@codengage.com>
2024-06-04 13:39:29 -0300

    test: adds tests for blog posts published helpers

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
-require "test_helper"
+require 'test_helper'
+class BlogPostTest < ActiveSupport::TestCase
+  test "draft? returns true for a draft blog_post" do
+    assert blog_posts(:draft).draft?
+  end
+  test "draft? returns false for a published blog_post" do
+    refute blog_posts(:published).draft?
+  end
+  test "draft? returns false for a scheduled blog_post" do
+    refute blog_posts(:scheduled).draft?
+  end
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "published? returns true for a published blog_post" do
+    assert blog_posts(:published).published?
+  end
+  test "published? returns false for a draft blog_post" do
+    refute blog_posts(:draft).published?
+  end
+  test "published? returns false for a scheduled blog_post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for a scheduled blog_post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+  test "scheduled? returns false for a draft blog_post" do
+    refute blog_posts(:draft).scheduled?
+  end
+  test "scheduled? returns false for a published blog_post" do
+    refute blog_posts(:published).scheduled?
+  end
 end
